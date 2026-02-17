afd46954ef3fe79ac40b46518f7404a4ad36f541
jayaram-outside <jayaram.maharjan@outside.studio>
2026-02-17 12:58:11 +0545

    data organization

--- main.js
+++ main.js
@@ -1,428 +1,35 @@
-// Product catalog - flat array with category included in each item
-const products = [
-  // Plates
-  {
-    category: "plates",
-    value: "diner-white-dinner",
-    name: "Diner White Dinner Plate",
-    price: "$14",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-PT11_3.png?v=1749591116&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-PT11_1_9e86e09e-81ff-4d80-84dd-9c87a3fe0fac.png?v=1749591116&width=1008",
-  },
-  {
-    category: "plates",
-    value: "spot-on-app-plate",
-    name: "Spot On - App Plate",
-    price: "$14",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SPO-PL08_03_26a2c202-edee-4682-bd8a-6d2c3d47dd60.png?v=1748451857&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SPO-PL08_01_2d06b658-b23a-48ce-881f-8f5a578e98ff.png?v=1748451858&width=1008",
-  },
-  {
-    category: "plates",
-    value: "greenband-main-plate",
-    name: "Greenband Main Plate",
-    price: "$14",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-PT11_03.png?v=1747320289&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-PT11_01_141c15a1-36a7-428d-a07a-54a343ef886c.png?v=1747320289&width=1008",
-  },
-  {
-    category: "plates",
-    value: "checkerboard-main-plate",
-    name: "Checkerboard Main Plate",
-    price: "$14",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-PT10_03.png?v=1746645275&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-PT10_01.png?v=1746645275&width=1008",
-  },
-  {
-    category: "plates",
-    value: "nyc-toile-dinner-plate",
-    name: "NYC Toile - Dinner Plate",
-    price: "$15",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-118_3.png?v=1763591816&width=823",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-118_1.png?v=1763591816&width=1008",
-  },
-  {
-    category: "plates",
-    value: "212-skyline-dinner",
-    name: "212 Skyline Dinner Plate",
-    price: "$18",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-212-PT08_03.png?v=1747940014&width=823",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-212-PT08_01.png?v=1747940015&width=1008",
-  },
-  {
-    category: "plates",
-    value: "brooklynese-plate",
-    name: "Pleasantries Dinner Plate",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BKN-BD08_2_0ea185be-acd7-4b15-a514-9e09a58310d8.png?v=1756489273&width=1066",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BKN-BD08_1_436fc719-b803-4552-9c10-14faa9bac124.png?v=1756489273&width=1008",
-  },
-  {
-    category: "plates",
-    value: "colorware-blue-dinner",
-    name: "Colorware Blue Dinner Plate",
-    price: "$15",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-175_3.png?v=1761841911&width=823",
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BKN-BD08_1_436fc719-b803-4552-9c10-14faa9bac124.png?v=1756489273&width=1008",
-  },
-  // Bowls
-  {
-    category: "bowls",
-    value: "everything-white-bowl",
-    name: "Everything Bowl White",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-BO22_3.png?v=1749591027&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-BO22_1_e1413a66-d56c-4cd0-bdaf-0a5816877294.png?v=1749591027&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "colorware-nut-bowl-cherry",
-    name: "Colorware - Nut Bowl Cherry",
-    price: "$14",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-122_1.png?v=1763146479&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-122_3.png?v=1763146480&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "greenband-bowl",
-    name: "Greenband Bowl",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-BO14_02.png?v=1747259382&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-BO14_03.png?v=1747259376&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "checkerboard-bowl",
-    name: "Checkerboard Bowl",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-BO14_02.png?v=1746645656&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-BO14_03.png?v=1746645653&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "skyline-bowl",
-    name: "Skyline Bowl",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-212-BO56_02.png?v=1761233878&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-212-BO22_03.png?v=1747939999&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "spot-on-bowl",
-    name: "Spot On - Bowl",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SPO-B056_03_5174c242-0b90-4f7a-9452-3af94dcb8d2c.png?v=1748451776&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SPO-B056_03_5174c242-0b90-4f7a-9452-3af94dcb8d2c.png?v=1748451776&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "nut-bowl-blue",
-    name: "Nut Bowl Coronet Blue",
-    price: "$6.50",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-128_1.png?v=1763146408&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-128_3.png?v=1763146408&width=1008",
-  },
-  {
-    category: "bowls",
-    value: "brooklyn-bowl",
-    name: "Brooklynese - Bowl",
-    price: "$6.50",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BKN-SB08_2_ca6f0807-7dba-4576-9666-04244343b476.png?v=1756489322&width=940",
-    topViewImage:"https://www.fishseddy.com/cdn/shop/files/FEST-BKN-SB08_3_ecd2726b-251b-4d97-9b81-96756412706a.png?v=1756489322&width=1008",
-  },
-  // Mugs
-  {
-    category: "mugs",
-    value: "bulldog-mug",
-    name: "Bull Dog Mug",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-182_1.png?v=1764712360&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-250-182_2.png?v=1764712360&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "spot-on-mug",
-    name: "Spot On - Mug",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SPO-MU12_01.png?v=1748451384&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SPO-MU12_03.png?v=1748451384&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "greenband-teacup",
-    name: "Greenband Tea Cup",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-MU07_01.png?v=1747259289&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-MU07_03.png?v=1747259289&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "checkerboard-dinner-mug",
-    name: "Checkerboard Dinner Mug",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-MU08_01.png?v=1746645190&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-MU08_03.png?v=1746645190&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "skyline-mug",
-    name: "Skyline Mug",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-212-MU08_1.png?v=1752174706&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-212-MU08_3.png?v=1752174706&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "cat-person-mug",
-    name: "Cat Person Mug",
-    price: "$16",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BEN-MUCP_1_0b403e89-7f8c-415b-b08f-b111f1b49963.png?v=1756142688&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BEN-MUCP_3_434522ee-eb8c-4f14-b7d8-21a70b3c0329.png?v=1756142688&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "pleasantries-mug",
-    name: "Pleasantries Mug",
-    price: "$14",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SLB-MU13_01.png?v=1746480310&width=400",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-LFC-MU08_03.png?v=1746645190&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "diner-white-mug",
-    name: "Diner White Mug",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-MU13_3.png?v=1749591135&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-MU13_3.png?v=1749591135&width=1008",
-  },
-  {
-    category: "mugs",
-    value: "brooklyn-mug",
-    name: "Brooklynese - Cawfee Mug",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BKN-MU10_2_205df4a0-6233-412c-820b-3f8cc242ce66.png?v=1756489349&width=1008",
+import * as anime from 'animejs';
 
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-BKN-MU10_3_012984a9-2e4b-4365-84a2-7bcc41dcb31b.png?v=1756489349&width=1008",
-  },
-  // Napkins
-  {
-    category: "napkins",
-    value: "sweet-tea-towel",
-    name: "Sweet Tea Towel",
-    price: "$8",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEKL-250-110_3.png?v=1764947422&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEKL-250-110_3.png?v=1764947422&width=1008",
-  },
-  {
-    category: "napkins",
-    value: "spot-on-guest-napkin",
-    name: "Spot On - Guest Napkin",
-    price: "$8",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEPG-NAP-SPO_1.png?v=1764788011&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEPG-NAP-SPO_1.png?v=1764788011&width=1008",
-  },
-  {
-    category: "napkins",
-    value: "skyline-tea-towel",
-    name: "Skyline - Tea towel",
-    price: "$8",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FECS-212-TT01_1.png?v=1750442020&width=400",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FECS-212-TT01_2.png?v=1750442021&width=1008",
-  },
-  {
-    category: "napkins",
-    value: "greenband-4-pack-napkin",
-    name: "Greenband Napkin",
-    price: "$10",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FECS-HLG-NA04_1.png?v=1752686632&width=400",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FECS-HLG-NA04_2.png?v=1752686632&width=1008",
-  },
-  {
-    category: "napkins",
-    value: "brooklyn-napkin",
-    name: "Brooklynese - Napkin",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FEKL-250-237_1_4755de54-a602-4a4d-a514-cfa8c9ae3ad0.png?v=1761334019&width=940",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEKL-250-237_1_4755de54-a602-4a4d-a514-cfa8c9ae3ad0.png?v=1761334019&width=1008",
-  },
-  {
-    category: "napkins",
-    value: "checkerboard-4-pack-napkin",
-    name: "Checkerboard Napkin",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/Checkeredtowel_1_935c3e9f-cbeb-4d55-8794-0c9bdbc6858f.png?v=1754928926&width=1008",
-  
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/Checkeredtowel_3.png?v=1769011221&width=1008",
-  },
-  {
-    category: "napkins",
-    value: "pedestrians-tea-towel",
-    name: "Pedestrians Tea Towel",
-    price: "$12",
-    image:
-      "https://www.fishseddy.com/cdn/shop/files/FECS-PED-TT01_2.png?v=1768489357&width=1008",
+// Import product data from external JSON file
+let products = [];
+let recommendedProducts = [];
 
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FECS-PED-TT01_1.png?v=1768489357&width=1008",
-  },
-];
+// Fetch and load the data
+fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    products = data.products;
+    recommendedProducts = data.recommendedProducts;
 
+    // Store in window object for global access
+    window.recommendedProducts = recommendedProducts;
+    
+    // Reinitialize any code that depends on products being loaded
+    initializeProductData();
+  })
+  .catch(error => console.error('Error loading product data:', error));
 
-
-// Bundle configurations
-const bundleData = {
-  "classic-diner": {
-    name: "Classic Diner Set",
-    products: {
-      plate: "diner-white-dinner",
-      bowl: "everything-white-bowl",
-      mug: "diner-white-mug",
-      napkin: "pedestrians-tea-towel",
-    },
-  },
-  "modern-minimalist": {
-    name: "Modern Minimalist",
-    products: {
-      plate: "brooklynese-plate",
-      bowl: "brooklyn-bowl",
-      mug: "brooklyn-mug",
-      napkin: "brooklyn-napkin",
-    },
-  },
-  "colorful-collection": {
-    name: "Colorful Collection",
-    products: {
-      plate: "colorware-blue-dinner",
-      bowl: "colorware-nut-bowl-cherry",
-      mug: "cat-person-mug",
-      napkin: "sweet-tea-towel",
-    },
-  },
-  "skyline-set": {
-    name: "NYC Skyline Set",
-    products: {
-      plate: "212-skyline-dinner",
-      bowl: "skyline-bowl",
-      mug: "skyline-mug",
-      napkin: "skyline-tea-towel",
-    },
-  },
-  "greenband-set": {
-    name: "Greenband Set",
-    products: {
-      plate: "greenband-main-plate",
-      bowl: "greenband-bowl",
-      mug: "greenband-teacup",
-      napkin: "greenband-4-pack-napkin",
-    },
-  },
-  "checkerboard-set": {
-    name: "Checkerboard Set",
-    products: {
-      plate: "checkerboard-main-plate",
-      bowl: "checkerboard-bowl",
-      mug: "checkerboard-dinner-mug",
-      napkin: "checkerboard-4-pack-napkin",
-    },
-  },
-  "spot-on-set": {
-    name: "Spot On Set",
-    products: {
-      plate: "spot-on-app-plate",
-      bowl: "spot-on-bowl",
-      mug: "spot-on-mug",
-      napkin: "spot-on-guest-napkin",
-    },
-  },
-};
+// Initialize products lookup and other dependent functionality
+function initializeProductData() {
+  // Rebuild the allProducts lookup since products were initially empty
+  Object.keys(allProducts).forEach(key => delete allProducts[key]);
+  products.forEach((product) => {
+    allProducts[product.value] = product;
+  });
+  
+  // Initialize recommendations now that data is loaded
+  initRecommendations();
+}
 
 // Create lookup map for easy product access by value
 const allProducts = {};
@@ -433,6 +40,19 @@ products.forEach((product) => {
 // Helper function to get products by category
 function getProductsByCategory(category) {
   return products.filter((p) => p.category === category);
+}
+
+// Helper function to get bundle product details
+function getBundleContents(bundleKey) {
+  const bundle = products.find((p) => p.type === "bundle" && p.value === bundleKey);
+  if (!bundle || !bundle.bundleContents) return null;
+
+  return {
+    ...bundle,
+    items: bundle.bundleContents
+      .map((item) => allProducts[item.productValue])
+      .filter(Boolean),
+  };
 }
 
 // View mode state - "front" or "top"
@@ -680,18 +300,19 @@ function createBundlePlaceholders() {
 }
 
 function applyBundle(bundleKey) {
-  const bundle = bundleData[bundleKey];
-  if (!bundle) return;
+  const bundleDetails = getBundleContents(bundleKey);
+  if (!bundleDetails || !bundleDetails.items) return;
 
   // Make sure bundle placeholders exist
   if (bundleSlotIds.length !== 4) createBundlePlaceholders();
 
   const [plateId, bowlId, mugId, napkinId] = bundleSlotIds;
+  const [plate, bowl, mug, napkin] = bundleDetails.items;
 
-  placeProductIntoSlot(plateId, bundle.products.plate);
-  placeProductIntoSlot(bowlId, bundle.products.bowl);
-  placeProductIntoSlot(mugId, bundle.products.mug);
-  placeProductIntoSlot(napkinId, bundle.products.napkin);
+  placeProductIntoSlot(plateId, plate?.value);
+  placeProductIntoSlot(bowlId, bowl?.value);
+  placeProductIntoSlot(mugId, mug?.value);
+  placeProductIntoSlot(napkinId, napkin?.value);
 }
 
 // Individual: Add Placeholder flow - now directly adds placeholder
@@ -833,59 +454,14 @@ function updateTableView() {
 // Recommendations
 function initRecommendations() {
   const slider = document.getElementById("recommendations-slider");
-  const recommendedProducts = [
-    {
-      name: "Butter Dish",
-      price: "$18",
-      image:
-        "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-BO56_2_9ded49f2-967e-4559-aef6-4fe67d6becd1.png?v=1749591189&width=200",
-    
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-BO56_2_9ded49f2-967e-4559-aef6-4fe67d6becd1.png?v=1749591189&width=200",
-  },
-    {
-      name: "Storage Bowl",
-      price: "$15",
-      image:
-        "https://www.fishseddy.com/cdn/shop/files/FEST-SLB-BO09_01.png?v=1746480280&width=200",
-    
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-SLB-BO09_01.png?v=1746480280&width=200",
-  },
-    {
-      name: "Coffee Cup",
-      price: "$14",
-      image:
-        "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-MU03_01.png?v=1747259346&width=200",
-    
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-HLG-MU03_01.png?v=1747259346&width=200",
-  },
-    {
-      name: "Serving Platter",
-      price: "$22",
-      image:
-        "https://www.fishseddy.com/cdn/shop/files/FEST-GOF-PT10_2.png?v=1753997374&width=1008",
-    
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-GOF-PT10_2.png?v=1753997374&width=1008",
-  },
-    {
-      name: "Salt & Pepper",
-      price: "$16",
-      image:
-        "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-PT11_1_9e86e09e-81ff-4d80-84dd-9c87a3fe0fac.png?v=1749591116&width=200",
-    
-    topViewImage:
-      "https://www.fishseddy.com/cdn/shop/files/FEST-DWH-PT11_1_9e86e09e-81ff-4d80-84dd-9c87a3fe0fac.png?v=1749591116&width=200",
-  },
-    {
-      name: "Tea Towel Set",
-      price: "$12",
-      image:
-        "https://www.fishseddy.com/cdn/shop/files/Checkeredtowel_2.png?v=1754928926&width=200",
-    },
-  ];
+  
+  // Load recommended products from the JSON data
+  const recommendedProducts = window.recommendedProducts || [];
+  
+  if (recommendedProducts.length === 0) {
+    console.warn('Recommended products not loaded yet');
+    return;
+  }
 
   slider.innerHTML = "";
   recommendedProducts.forEach((product) => {
@@ -912,7 +488,6 @@ function slideRight() {
   slider.scrollBy({ left: 220, behavior: "smooth" });
 }
 
-initRecommendations();
 // Ensure table view matches initial `currentView`
 updateTableView();
 
